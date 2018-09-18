a4fe6c935ee55f4163febe6c5ed376f8139b924f
ermakov4as <ermakov4as@gmail.com>
2018-09-19 02:27:05 +0300

    v0.0.1

---
+++ src/components/PageTitleBar/PageTitleBarMod.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-container fluid>
+		<div>
+		<app-card :fullBlock="true">
+			<v-breadcrumbs style="padding: 0">
+				<div style="width: 100% ">
+					<v-btn flat large disabled class="btn-left">Дисциплины</v-btn>
+					<v-btn flat large disabled class="btn-right">Я пользователь</v-btn>
+				</div>
+			</v-breadcrumbs>
+			<v-breadcrumbs style="text-align: center">
+				<h2 style="width: 100%; font-size: 36px">{{$t(pageTitle)}}</h2>
+			</v-breadcrumbs>
+		</app-card>
+		</div>
+	</v-container>
+</template>
+
+<script>
+export default {
+  computed: {
+    // computed property to get page breadcrumbs
+    pageTitle() {
+      return this.$breadcrumbs[0].meta.title;
+    },
+    pageBreadcrumb() {
+      return this.$breadcrumbs[0].meta.breadcrumb;
+    }
+  }
+};
+</script>
